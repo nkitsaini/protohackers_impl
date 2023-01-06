dec2745f5b096210632d691999013f6c4509b3b6
Ankit Saini <nnkitsaini@gmail.com>
2023-01-06 12:36:59 +0530

    temp

--- p2_means/src/main.rs
+++ p2_means/src/main.rs
@@ -1,3 +1,23 @@
+struct I32 {
+    val: i32
+}
+struct InsertMessage {
+    timestamp: I32,
+    price: I32
+}
+struct QueryMessage {
+    mintime: I32,
+    maxtime: I32
+}
+
+enum Message {
+    Insert(InsertMessage),
+    Query(QueryMessage)
+}
+
+// impl 
+
+
 fn main() {
-    println!("Hello, world!");
+
 }
