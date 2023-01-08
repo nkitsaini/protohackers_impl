fafd15ed3e449e181f1317f82403a40bd5f9f99a
Ankit Saini <nnkitsaini@gmail.com>
2023-01-08 22:24:04 +0530

    fmt

--- src/p8_insecure.rs
+++ src/p8_insecure.rs
@@ -1,65 +1,65 @@
 use std::num::Wrapping;
 
-use anyhow::{Context, bail};
+use anyhow::{bail, Context};
 use tokio::io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt};
 
 #[derive(Debug)]
 enum CipherOp {
-	ReverseBit,
-	Xor(u8),
-	XorPos,
-	Add(u8),
-	AddPos
+    ReverseBit,
+    Xor(u8),
+    XorPos,
+    Add(u8),
+    AddPos,
 }
 
 impl CipherOp {
-	fn encode(&self, pos_end: u8, bt: u8) -> u8 {
-		let wbt = Wrapping(bt);
-		let wpe = Wrapping(pos_end);
-		match self {
-			Self::ReverseBit => bt.reverse_bits(),
-			Self::Xor(v) => bt^v,
-			Self::XorPos => bt^pos_end,
-			Self::Add(v) => (wbt+Wrapping(*v)).0,
-			Self::AddPos => (wbt+wpe).0,
-		}
-	}
+    fn encode(&self, pos_end: u8, bt: u8) -> u8 {
+        let wbt = Wrapping(bt);
+        let wpe = Wrapping(pos_end);
+        match self {
+            Self::ReverseBit => bt.reverse_bits(),
+            Self::Xor(v) => bt ^ v,
+            Self::XorPos => bt ^ pos_end,
+            Self::Add(v) => (wbt + Wrapping(*v)).0,
+            Self::AddPos => (wbt + wpe).0,
+        }
+    }
 
-	fn decode(&self, pos_end: u8, bt: u8) -> u8 {
-		let wbt = Wrapping(bt);
-		let wpe = Wrapping(pos_end);
-		match self {
-			Self::ReverseBit => bt.reverse_bits(),
-			Self::Xor(v) => bt^v,
-			Self::XorPos => bt^pos_end,
-			Self::Add(v) => (wbt-Wrapping(*v)).0,
-			Self::AddPos => (wbt-wpe).0,
-		}
-	}
+    fn decode(&self, pos_end: u8, bt: u8) -> u8 {
+        let wbt = Wrapping(bt);
+        let wpe = Wrapping(pos_end);
+        match self {
+            Self::ReverseBit => bt.reverse_bits(),
+            Self::Xor(v) => bt ^ v,
+            Self::XorPos => bt ^ pos_end,
+            Self::Add(v) => (wbt - Wrapping(*v)).0,
+            Self::AddPos => (wbt - wpe).0,
+        }
+    }
 
-	fn parse_all(buf: Vec<u8>) -> anyhow::Result<Vec<Self>> {
-		let mut i = 0;
-		let mut rv = vec![];
-		while i < buf.len() {
-			rv.push(match buf[i] {
-				0x01 => Self::ReverseBit,
-				0x02 => {
-					i += 1;
-					Self::Xor(*buf.get(i).context("invalid")?)
-				},
-				0x03 => Self::XorPos,
-				0x04 => {
-					i += 1;
-					
-					Self::Add(*buf.get(i).context("invalid")?)
-				},
-				0x05 => Self::AddPos,
-				_ => bail!("Unknown cipher type")
-			});
-			i += 1;
-		}
-		Ok(rv)
-	}
+    fn parse_all(buf: Vec<u8>) -> anyhow::Result<Vec<Self>> {
+        let mut i = 0;
+        let mut rv = vec![];
+        while i < buf.len() {
+            rv.push(match buf[i] {
+                0x01 => Self::ReverseBit,
+                0x02 => {
+                    i += 1;
+                    Self::Xor(*buf.get(i).context("invalid")?)
+                }
+                0x03 => Self::XorPos,
+                0x04 => {
+                    i += 1;
+
+                    Self::Add(*buf.get(i).context("invalid")?)
+                }
+                0x05 => Self::AddPos,
+                _ => bail!("Unknown cipher type"),
+            });
+            i += 1;
+        }
+        Ok(rv)
+    }
 }
 
 #[derive(Debug)]
@@ -67,34 +67,36 @@ struct Cipher {
     operations: Vec<CipherOp>,
 }
 impl Cipher {
-	fn is_dumb(&self) -> bool {
-		for pos in 0x00..0xff {
-			for bt in 0x00..0xff {
-				if self.encode(pos, bt) != bt {
-					return false
-				}
-			}
-		}
-		true
-	}
+    fn is_dumb(&self) -> bool {
+        for pos in 0x00..0xff {
+            for bt in 0x00..0xff {
+                if self.encode(pos, bt) != bt {
+                    return false;
+                }
+            }
+        }
+        true
+    }
     fn new(cipher: Vec<u8>) -> anyhow::Result<Self> {
-        Ok(Self { operations: CipherOp::parse_all(cipher)? })
+        Ok(Self {
+            operations: CipherOp::parse_all(cipher)?,
+        })
     }
     fn decode(&self, pos: usize, val: u8) -> u8 {
-		let mut rv = val;
-		let pos_end = (pos & 0xff) as u8;
-		for cipher in self.operations.iter().rev() {
-			rv = cipher.decode(pos_end, rv);
-		}
-		rv
+        let mut rv = val;
+        let pos_end = (pos & 0xff) as u8;
+        for cipher in self.operations.iter().rev() {
+            rv = cipher.decode(pos_end, rv);
+        }
+        rv
     }
     fn encode(&self, pos: usize, val: u8) -> u8 {
-		let mut rv = val;
-		let pos_end = (pos & 0xff) as u8;
-		for cipher in self.operations.iter() {
-			rv = cipher.encode(pos_end, rv);
-		}
-		rv
+        let mut rv = val;
+        let pos_end = (pos & 0xff) as u8;
+        for cipher in self.operations.iter() {
+            rv = cipher.encode(pos_end, rv);
+        }
+        rv
     }
 }
 
@@ -108,7 +110,14 @@ impl ToyInfo {
     pub fn new(content: &str) -> anyhow::Result<Self> {
         Ok(Self {
             text: content.to_string(),
-            count: regex::Regex::new(r#"(^\d+)x"#).unwrap().captures(content).context("no number at start")?.get(1).unwrap().as_str().parse()?,
+            count: regex::Regex::new(r#"(^\d+)x"#)
+                .unwrap()
+                .captures(content)
+                .context("no number at start")?
+                .get(1)
+                .unwrap()
+                .as_str()
+                .parse()?,
         })
     }
 }
@@ -120,55 +129,55 @@ async fn handle_client(sock: tokio::net::TcpStream) -> anyhow::Result<()> {
     let mut buf = vec![];
     reader.read_until(0x00, &mut buf).await?;
 
-	let mut res_pos = 0;
+    let mut res_pos = 0;
     let mut prev_buf = String::new();
-	let mut req_pos = 0;
+    let mut req_pos = 0;
 
     buf.pop();
     let cipher = Cipher::new(buf)?;
-	if cipher.is_dumb() {
-		bail!("Using a dumb cipher");
-	}
-
+    if cipher.is_dumb() {
+        bail!("Using a dumb cipher");
+    }
 
     loop {
-		while let Some(i) = prev_buf.find('\n') {
-			let pb = prev_buf.clone();
-			let (mut line, extra) = pb.split_at(i+1);
-			prev_buf = extra.to_string();
-			line = &line[..i];
-			if line.len() == 0 {
-				bail!("Empty line");
-			}
-			let toys = line.split(',').into_iter().map(|x| ToyInfo::new(x)).collect::<anyhow::Result<Vec<_>>>()?;
-			let best_toy = toys
-				.iter()
-				.max_by(|a, b| a.count.cmp(&b.count))
-				.unwrap();
-			
-			let mut resp_bytes = best_toy.text.as_bytes().to_vec();
-			resp_bytes.push('\n' as u8);
-			let mut encoded_resp_bytes = vec![];
-			for (i, bt) in resp_bytes.into_iter().enumerate() {
-				let pos = i + res_pos;
-				encoded_resp_bytes.push(cipher.encode(pos, bt));
-			}
-			res_pos += &encoded_resp_bytes.len();
-			wh.write_all(&encoded_resp_bytes).await?;
-		}
+        while let Some(i) = prev_buf.find('\n') {
+            let pb = prev_buf.clone();
+            let (mut line, extra) = pb.split_at(i + 1);
+            prev_buf = extra.to_string();
+            line = &line[..i];
+            if line.len() == 0 {
+                bail!("Empty line");
+            }
+            let toys = line
+                .split(',')
+                .into_iter()
+                .map(|x| ToyInfo::new(x))
+                .collect::<anyhow::Result<Vec<_>>>()?;
+            let best_toy = toys.iter().max_by(|a, b| a.count.cmp(&b.count)).unwrap();
+
+            let mut resp_bytes = best_toy.text.as_bytes().to_vec();
+            resp_bytes.push('\n' as u8);
+            let mut encoded_resp_bytes = vec![];
+            for (i, bt) in resp_bytes.into_iter().enumerate() {
+                let pos = i + res_pos;
+                encoded_resp_bytes.push(cipher.encode(pos, bt));
+            }
+            res_pos += &encoded_resp_bytes.len();
+            wh.write_all(&encoded_resp_bytes).await?;
+        }
         let mut line = vec![0u8; 1024];
         let read_len = reader.read(&mut line).await?;
-		line = line[..read_len].to_vec();
-		if line.len() == 0 {
-			bail!("EOF from client");
-		}
+        line = line[..read_len].to_vec();
+        if line.len() == 0 {
+            bail!("EOF from client");
+        }
         let mut decoded_line = vec![];
         for (i, bt) in line.into_iter().enumerate() {
             let pos = i + req_pos;
             decoded_line.push(cipher.decode(pos, bt));
         }
-		req_pos += decoded_line.len();
-		prev_buf += &String::from_utf8(decoded_line)?;
+        req_pos += decoded_line.len();
+        prev_buf += &String::from_utf8(decoded_line)?;
     }
     Ok(())
 }
@@ -181,7 +190,6 @@ async fn run() -> anyhow::Result<()> {
         let (socket, _) = tcp_conn.accept().await?;
         tokio::spawn(async move { dbg!(handle_client(socket).await) });
         client_id += 1;
-
     }
     Ok(())
 }
@@ -209,25 +217,26 @@ mod tests {
             .await?
             .into_split();
 
-
         let mut reader = tokio::io::BufReader::new(read_half1);
 
         let msg1 = vec![0x02, 0x7b, 0x05, 0x01, 0x00];
         write_half1.write_all(&msg1).await?;
-		
-        let msg1 = vec![0xf2, 0x20, 0xba, 0x44, 0x18, 0x84, 0xba, 0xaa, 0xd0, 0x26, 0x44, 0xa4, 0xa8, 0x7e];
+
+        let msg1 = vec![
+            0xf2, 0x20, 0xba, 0x44, 0x18, 0x84, 0xba, 0xaa, 0xd0, 0x26, 0x44, 0xa4, 0xa8, 0x7e,
+        ];
         write_half1.write_all(&msg1).await?;
 
-		let mut rlines = reader.lines();
-		dbg!("Going to wait");
-        tokio::select!{
+        let mut rlines = reader.lines();
+        dbg!("Going to wait");
+        tokio::select! {
             l = rlines.next_line() => {
-				dbg!("Got resp");
+                dbg!("Got resp");
                 dbg!(l.unwrap().unwrap());
                 assert!(false);
             },
             _ = tokio::time::sleep(std::time::Duration::from_secs(1)) => {
-			dbg!("Timed out");
+            dbg!("Timed out");
                 anyhow::bail!("Timed out read");
             }
         }
